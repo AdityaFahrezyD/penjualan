562f442452161a139acb0449c682318effd2fcd4
Aditya Fahrezy <aditgamer01@gmail.com>
2026-09-07 09:14:44 +0700

    cors

--- config/cors.php
+++ config/cors.php
@@ -22,8 +22,8 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'https://sxvx55d0-8000.asse.devtunnels.ms/',
-        'https://transaksi-delta.vercel.app/'
+        'https://sxvx55d0-8000.asse.devtunnels.ms',
+        'https://transaksi-delta.vercel.app'
     ],
 
     'allowed_origins_patterns' => [],
